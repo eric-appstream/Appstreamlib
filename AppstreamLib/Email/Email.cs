0e537d9a06d5231c8b0e6a6395f5d20bc999a511
Eric Chong <eric@appstream.com.my>
2017-04-17 15:44:58 +0800

    first push

---
+++ AppstreamLib/Email/Email.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.Configuration;
+
+namespace AppstreamLib.Email
+{
+    public class Email
+    {
+        public static void SendEmail(string destination_email, string content , string subject , bool isEnableSSL = false , bool isBodyHtml = false)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = ConfigurationManager.AppSettings["Email.Host"];
+            smtpClient.Port = int.Parse(ConfigurationManager.AppSettings["Email.Port"]);
+            smtpClient.EnableSsl = isEnableSSL;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["Email.Sender"], ConfigurationManager.AppSettings["Email.Sender.Password"]);
+            System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+            email.From = new MailAddress(ConfigurationManager.AppSettings["Email.Sender"], ConfigurationManager.AppSettings["Email.Sender.Name"]);
+            email.To.Add(destination_email);
+            email.Subject = subject;
+            email.IsBodyHtml = isBodyHtml;
+            email.Body += content;
+
+            try
+            {
+                smtpClient.Send(email);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+
+
+}
---
+++ AppstreamLib/SMS/SMS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using RestSharp;
+
+namespace AppstreamLib.SMS
+{
+    public class SMS
+    {
+        public static void SendSMS( string phonenum, string message)
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["sms.client"]);
+
+            var request = new RestRequest(ConfigurationManager.AppSettings["sms.endpoint"], Method.POST);
+            request.AddQueryParameter("user", ConfigurationManager.AppSettings["sms.user"]);
+            request.AddQueryParameter("pass", ConfigurationManager.AppSettings["sms.password"]);
+            request.AddQueryParameter("type", "0"); //ascii
+            request.AddQueryParameter("to", phonenum);  //receiver phone num 
+            request.AddQueryParameter("from", ConfigurationManager.AppSettings["sms.sender"]);   //sender name
+            request.AddQueryParameter("text", message);
+            request.AddQueryParameter("servid", ConfigurationManager.AppSettings["sms.servid"]);   //service id
+
+            var response = client.Execute(request);
+            var content = response.Content; // raw content as string
+            var statuscode = response.StatusCode;
+
+            if (statuscode != System.Net.HttpStatusCode.OK || response.Content == "401")
+            {
+                throw new Exception(response.ErrorMessage);
+            }
+
+        }
+    }
+}
